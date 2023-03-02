c2b6367e3d7d3b0b2ac912b780fd2eecf209f114
rencoo <rencoogle@outlook.com>
2023-03-02 09:49:11 +0800

    feat: 生命周期
    * componentDidMount 仅触发一次
    * componentDidUpdate 每次更新都会触发

---
+++ lifecycle/src/index.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { log } from '../../utils/index'
+
+export default class Clock extends React.Component {
+  constructor(props) {
+    super(props)
+    log('constructor invoked')
+    this.timerID = null // TODO
+    this.state = {
+      data: new Date()
+    }
+  }
+
+  componentDidMount() {
+    log('componentDidMount invoked')
+    this.timerID = setInterval(() => this.tick(), 1000)
+  }
+
+  componentWillUnmount() {
+    log('componentWillUnmount invoked')
+    clearInterval(this.timerID)
+  }
+
+  componentDidUpdate() {
+    log('componentDidUpdate invoked')
+  }
+
+  // methods
+  tick() {
+    this.setState({
+      data: new Date()
+    })
+  }
+
+  render() {
+    const { data } = this.state
+    return (
+      <div>
+        <h2>
+          It is { data.toLocaleTimeString() }
+        </h2>
+      </div>
+    )
+  }
+
+}
+
+function App() {
+  return (<>
+    <Clock />
+  </>)
+}
+
+// ========================================
+const rootElement = document.getElementById('app');
+// ReactDOM.render(<App />, rootElement)
+ReactDOM.render(<App />, rootElement);
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 /** @type {import('webpack').Configuration} */
 module.exports = {
   mode: 'none',
-  entry: './compare/src/index.js',
+  entry: './lifecycle/src/index.js',
   module: {
     rules: [
       {
