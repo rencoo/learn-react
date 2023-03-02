bcdab13185774a2ea9cdd28c9dd629df11caa131
rencoo <rencoogle@outlook.com>
2023-03-02 20:37:40 +0800

    feat: ahooks
    * useAntdTable
    * useAntdTable 基于 useRequest 实现，封装了常用的 Ant Design Form 与 Ant Design Table 联动逻辑，并且同时支持 antd v3 和 v4

--- ahook/index.tsx
+++ ahook/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App.tsx';
+import App from './App2.tsx';
 
 const rootElement = document.getElementById('app');
 ReactDOM.render(
